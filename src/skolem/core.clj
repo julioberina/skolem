ac4d1e54b1f18c66bac2d35a6480687cd2df6059
Julio Berina <julioberina@gmail.com>
2016-02-11 17:14:16 -0800

    Implemented find-zeros function and added support for big ints

--- src/skolem/core.clj
+++ src/skolem/core.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 (defn next-term [terms k coefficients]
-  (apply + (map (fn [x] (eval (conj x '*)))
+  (apply +' (map (fn [x] (eval (conj x '*')))
   (partition 2 (interleave (nthrest (reverse terms) (dec k)) coefficients)))))
 
 (defn shift-terms [terms num] (conj (vec (rest terms)) num))
@@ -16,7 +16,11 @@
       (lazy-seq (cons (first terms) (lrr (shift-terms terms a_n) k)))
       (lazy-seq (cons (first terms) (lrr (shift-terms terms a_n) k (first coefficients)))))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn find-zeros [sequence]
+  "Finds the indexes of the zeroes in the function"
+  (loop [iteration 0 result []]
+    (if (> iteration (count sequence)) result
+    	(recur (inc iteration) (conj result iteration)))))
+
+(defn -main [& args]
+  (println "Coming soon..."))
