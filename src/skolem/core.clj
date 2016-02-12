b1a77320b1c5d58b833907dd3760cc59dc2b0bce
Julio Berina <julioberina@gmail.com>
2016-02-11 16:17:51 -0800

    Shortened code and converted lrr to a lazy seq function

--- src/skolem/core.clj
+++ src/skolem/core.clj
@@ -1,47 +1,20 @@
 (ns skolem.core
   (:gen-class))
 
-(defn lrr
-  "Generates a linear recurrence relation associated with details of the equation a[n]"
-  ;; If all coefficients are 1
-  ([seqlen initnums param_k]
-    {:pre [(and
-    	    (vector? initnums)
-	    (every? integer? initnums)
-	    (>= seqlen (count initnums))
-	    (> param_k 0))]}
-    (let [n (atom (count initnums))
-    	  k (atom param_k)
-	  value (atom 0)
-    	  result (atom initnums)]
-      (while (< @n seqlen)
-        (dotimes [i (- (count initnums) (dec param_k))]
-          (swap! value + (get @result (- @n @k)))
-	  (swap! k inc))
-        (swap! result conj @value)
-	(swap! n inc)
-	(reset! k param_k) (reset! value 0))
-	(seq @result)))
-	
-  ;; If some have different coefficients
-  ([seqlen initnums param_k coefficients]
-    {:pre [(and
-           (vector? initnums)
-	   (every? integer? initnums)
-	   (>= seqlen (count initnums))
-	   (> param_k 0))]}
-    (let [n (atom (count initnums))
-    	  k (atom param_k)
-	  value (atom 0)
-	  result (atom initnums)]
-      (while (< @n seqlen)
-        (dotimes [i (- (count initnums) (dec param_k))]
-	  (swap! value + (* (get @result (- @n @k)) (get coefficients i)))
-	  (swap! k inc))
-	(swap! result conj @value)
-	(swap! n inc)
-	(reset! k param_k) (reset! value 0))
-	(seq @result))))
+(defn next-term [terms k coefficients]
+  (apply + (map (fn [x] (eval (conj x '*)))
+  (partition 2 (interleave (nthrest terms (dec k)) coefficients)))))
+
+(defn shift-terms [terms num] (conj (vec (rest terms)) num))
+
+(defn lrr [terms k & coefficients]
+  "Generates a linear recurrence relation using initial terms, k, and optional coefficients"
+  (let [a_n (if (empty? coefficients)
+       	    (next-term terms k (take (count terms) (repeat 1)))
+	    (next-term terms k (take (count terms) (first coefficients))))]
+    (if (empty? coefficients)
+      (lazy-seq (cons (first terms) (lrr (shift-terms terms a_n) k)))
+      (lazy-seq (cons (first terms) (lrr (shift-terms terms a_n) k (first coefficients)))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
