fca81ab535e3ee57fcb30172234f604a59d49faa
Julio Berina <julioberina@gmail.com>
2016-02-12 19:31:16 -0800

    Working on main function

--- src/skolem/core.clj
+++ src/skolem/core.clj
@@ -7,8 +7,11 @@
 
 (defn shift-terms [terms num] (conj (vec (rest terms)) num))
 
+(defn error [] (println "Program exited due to error in input"))
+
 (defn lrr [terms k & coefficients]
-  "Generates a linear recurrence relation using initial terms, k, and optional coefficients"
+  "Generates a linear recurrence relation using initial terms, k, and optional coefficients.\n
+  Equation format: a_n = c_0*a_(n-k) + c_1*a_(n-(k+1)) + ... + c_(n-1)*a_0\n"
   (let [a_n (if (empty? coefficients)
        	    (next-term terms k (take (count terms) (repeat 1)))
 	    (next-term terms k (take (count terms) (first coefficients))))]
@@ -16,11 +19,39 @@
       (lazy-seq (cons (first terms) (lrr (shift-terms terms a_n) k)))
       (lazy-seq (cons (first terms) (lrr (shift-terms terms a_n) k (first coefficients)))))))
 
-(defn find-zeros [sequence]
-  "Finds the indexes of the zeroes in the function"
-  (loop [iteration 0 result []]
-    (if (> iteration (count sequence)) result
-    	(recur (inc iteration) (conj result iteration)))))
+(defn skolemization [sequence]
+  "Finds the index of the zero in the linear recurrence relation"
+  (loop [result [] it 0]
+    (if (>= it (count sequence)) result
+      (if (zero? (nth sequence it))
+        (recur (conj result it) (inc it))
+	(recur result (inc it))))))
 
 (defn -main [& args]
-  (println "Coming soon..."))
+  (print "The Skolem Problem Program\n\n")
+  (print "Equation format: a_n = c_0*a_(n-k) + c_1*a_(n-(k+1)) + ... + c_(n-1)*a_0\n\n")
+  (let [attr (atom {:terms [] :k 0 :coefficients '()})]
+    (print "Enter initial values: ") (flush)
+    (swap! attr assoc :terms (vec (map #(read-string %) (re-seq #"\S+" (clojure.string/replace (read-line) "," "")))))
+    (print "Enter k: ") (flush)
+    (swap! attr assoc :k (read-string (read-line)))
+    (print "Are all coefficients 1? (y/n): ") (flush)
+    (cond
+      (or (= (read-line) "y") (= (read-line) "Y"))
+      (do (print "How many terms would you like? ") (flush)
+      (let [number (read-string (read-line))]
+        (print "\nSequence: ") (doseq [i (take number (lrr (:terms @attr) (:k @attr)))] (print i)) (print "\n")
+	(print "Zeroes: ") (doseq [i (skolemization (take number (lrr (:terms @attr) (:k @attr))))] (print i " ")) (print "\n\n"))
+      (print "Would you like to redo? ") (flush)
+      (if (or (= (read-line) "y") (= (read-line) "Y")) (do (print "\n\n\n") (-main))
+      (print "\n"))) 
+      (or (= (read-line) "n") (= (read-line) "N"))
+      (do (print "Write out coefficients in order: ") (*flush-on-newline*)
+      (swap! attr assoc :coefficients (map #(read-string %) (re-seq #"\S+" (clojure.string/replace (read-line) "," ""))))
+      (print "How many terms would you like? ") (flush)
+      (let [number (read-string (read-line))]
+        (print "\nSequence: ") (doseq [i (take number (lrr (:terms @attr) (:k @attr) (:coefficients @attr)))] (print i))
+	(print "\nZeroes: ") (doseq [i (skolemization (take number (lrr (:terms @attr) (:k @attr) (:coefficients @attr))))] (print i " ")) (print "\n\n"))
+      (print "Would you like to redo? ") (flush)
+      (if (or (= (read-line) "y") (= (read-line) "Y")) (do (print "\n\n\n") (-main))
+      (print "\n"))))))
